825a4f85a3d9b41f06935bdd466af6f44a66c42c
Bidji <159267099+weBidji@users.noreply.github.com>
2024-03-28 15:46:09 -0400

    basic game logic, must fix tie check

---
+++ script.js
@@ -0,0 +1,75 @@
+const game = (function () {
+    let board = new Array(9);
+
+    const player1 = {
+        name: 'Player 1',
+        marker: 'X'
+    };
+
+    const player2 = {
+        name: 'Player 2',
+        marker: 'O'
+    };
+
+    let isP1Turn = true;
+
+    const playTurn = function (position) {
+        if (position < 0 || position > 8) {
+            console.log('Invalid position.');
+        }
+        else if (board[position] !== undefined) {
+            console.log('Position already taken');
+        } else {
+            const currentPlayer = isP1Turn ? player1 : player2;
+            board[position] = currentPlayer.marker;
+            console.log(currentPlayer.marker + ' placed on cell ' + position + '.');
+            switchTurn();
+            const result = checkForWin();
+            if (result === 'tie') {
+                // console.log("It's a tie!");
+            } else if (result) {
+                console.log(currentPlayer.name + ' wins!');
+
+            }
+        }
+    };
+
+
+
+    const switchTurn = function () {
+        isP1Turn = !isP1Turn;
+    };
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const checkForWin = function () {
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return true;
+            }
+        }
+
+
+        if (board.every(cell => cell === 'X' || cell === 'O')) {
+            console.log('tied')
+            return 'tie';
+        }
+
+        return false;
+    };
+
+    return {
+        playTurn: playTurn,
+        switchTurn: switchTurn
+    };
+})();
